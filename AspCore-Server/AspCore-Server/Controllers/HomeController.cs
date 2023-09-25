9c67e565f2c6c229177a50f70daccf14f5f857d7
sivakumar3442 <sivakumar.sf3442@gmail.com>
2023-09-25 13:43:59 +0530

    Sample changes added

---
+++ AspCore-Server/AspCore-Server/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCore_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class HomeController : Controller
+    {
+        public IWebHostEnvironment hostingEnvironment;
+        public HomeController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+        [Route("Download")]
+        public FileStreamResult Download()
+        {
+            string fullPath = this.hostingEnvironment.ContentRootPath + "\\wwwroot\\2.png";
+            FileStream fileStreamInput = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+            FileStreamResult fileStreamResult = new FileStreamResult(fileStreamInput, "APPLICATION/octet-stream");
+            fileStreamResult.FileDownloadName = "Custom.png";
+            return fileStreamResult;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Firefox-Reconnecting-issue
+
+Make sure to start the ASP.NET Core server application first, as the Blazor Server application relies on it to serve download responses.
